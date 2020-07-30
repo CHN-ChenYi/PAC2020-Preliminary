20225cd000e65f106cc1898b7609eac15d2d9ce9
TO/GA <chenyi2000@zju.edu.cn>
2020-07-30 22:35:56 +0800

    change double to float

--- checker.cpp
+++ checker.cpp
@@ -7,17 +7,20 @@ const int K = 100000;   // DO NOT CHANGE!!
 
 char colon;
 double x, y;
+double max_de, de;
 
 int main() {
   ifstream std("check.dat"), result("result.dat");
   for (int i = 0, _; i < K; i++) {
     std >> _ >> colon >> x;
     result >> _ >> colon >> y;
-    if (abs((y - x) / x * 100000) > 1) {
-      cout << i + 1 << ": should be " << x << " but read " << y << endl;
+    de = abs((y - x) / x * 100000);
+    max_de = max_de > de ? max_de : de;
+    if (de > 1) {
+      cout << i + 1 << ": should be " << x << " but read " << y << '(' << de << ')' << endl;
       return 1;
     }
   }
-  cout << "success" << endl;
+  cout << "success, max de: " << max_de << endl;
   return 0;
 }
--- main.cpp
+++ main.cpp
@@ -15,12 +15,11 @@ using namespace std;
 
 typedef chrono::high_resolution_clock Clock;
 
-const int m = 1638400; // DO NOT CHANGE!!
-const int K = 100000;  // DO NOT CHANGE!!
+const int m = 1638400;  // DO NOT CHANGE!!
+const int K = 100000;   // DO NOT CHANGE!!
 int mpi_id, mpi_size, block_size;
 size_t full_size, remainder_size;
-enum MPI_TAG
-{
+enum MPI_TAG {
   kDatReal,
   kDatImag,
   kPriReal,
@@ -31,21 +30,17 @@ enum MPI_TAG
   kAns
 };
 MPI_Status mpi_status;
-double *dat_real, *dat_imag, *pri_real, *pri_imag, *ctf, *sigRcp, *disturb,
-    *ans;
+float *dat_real, *dat_imag, *pri_real, *pri_imag, *ctf, *sigRcp, *disturb, *ans;
 
-inline double pow_2(const double &x) { return x * x; }
+inline float pow_2(const float &x) { return x * x; }
 
-inline double logDataVSPrior(const double *dat_real, const double *dat_imag,
-                             const double *pri_real, const double *pri_imag,
-                             const double *ctf, const double *sigRcp,
-                             const int num, const double disturb0)
-{
-  double result = 0.0;
-#pragma omp parallel for reduction(+ \
-                                   : result) schedule(static)
-  for (int i = 0; i < num; i++)
-  {
+inline float logDataVSPrior(const float *dat_real, const float *dat_imag,
+                            const float *pri_real, const float *pri_imag,
+                            const float *ctf, const float *sigRcp,
+                            const int num, const float disturb0) {
+  float result = 0.0;
+#pragma omp parallel for reduction(+ : result) schedule(static)
+  for (int i = 0; i < num; i++) {
     result += (pow_2(dat_real[i] - disturb0 * ctf[i] * pri_real[i]) +
                pow_2(dat_imag[i] - disturb0 * ctf[i] * pri_imag[i])) *
               sigRcp[i];
@@ -53,12 +48,10 @@ inline double logDataVSPrior(const double *dat_real, const double *dat_imag,
   return result;
 }
 
-inline void Server()
-{
+inline void Server() {
   MPI_Request mpi_request[K];
-  double *tmp_ans[mpi_size];
-  for (int i = 1; i < mpi_size; i++)
-    tmp_ans[i] = new double[K];
+  float *tmp_ans[mpi_size];
+  for (int i = 1; i < mpi_size; i++) tmp_ans[i] = new float[K];
 
   /***************************
    * Read data from input.dat
@@ -66,35 +59,29 @@ inline void Server()
   ifstream fin;
 
   fin.open("input.dat");
-  if (!fin.is_open())
-  {
+  if (!fin.is_open()) {
     cout << "Error opening file input.dat" << endl;
     exit(1);
   }
   int i = 0;
-  while (!fin.eof())
-  {
+  while (!fin.eof()) {
     fin >> dat_real[i] >> dat_imag[i] >> pri_real[i] >> pri_imag[i] >> ctf[i] >>
         sigRcp[i];
     i++;
-    if (i == m)
-      break;
+    if (i == m) break;
   }
   fin.close();
 
   fin.open("K.dat");
-  if (!fin.is_open())
-  {
+  if (!fin.is_open()) {
     cout << "Error opening file K.dat" << endl;
     exit(1);
   }
   i = 0;
-  while (!fin.eof())
-  {
+  while (!fin.eof()) {
     fin >> disturb[i];
     i++;
-    if (i == K)
-      break;
+    if (i == K) break;
   }
   fin.close();
 
@@ -104,12 +91,12 @@ inline void Server()
   auto startTime = Clock::now();
 
   for (int i = 1; i < mpi_size; i++)
-    MPI_Send(disturb, K, MPI_DOUBLE, i, kDisturb, MPI_COMM_WORLD);
-#define SEND(POINTER, TAG)                                                     \
-  for (int i = 1; i < mpi_size - 1; i++)                                       \
-    MPI_Send(POINTER + i * block_size, block_size, MPI_DOUBLE, i, TAG,         \
-             MPI_COMM_WORLD);                                                  \
-  MPI_Send(POINTER + full_size, remainder_size, MPI_DOUBLE, mpi_size - 1, TAG, \
+    MPI_Send(disturb, K, MPI_FLOAT, i, kDisturb, MPI_COMM_WORLD);
+#define SEND(POINTER, TAG)                                                    \
+  for (int i = 1; i < mpi_size - 1; i++)                                      \
+    MPI_Send(POINTER + i * block_size, block_size, MPI_FLOAT, i, TAG,         \
+             MPI_COMM_WORLD);                                                 \
+  MPI_Send(POINTER + full_size, remainder_size, MPI_FLOAT, mpi_size - 1, TAG, \
            MPI_COMM_WORLD);
   SEND(dat_real, kDatReal);
   SEND(dat_imag, kDatImag);
@@ -126,8 +113,7 @@ inline void Server()
   fout.rdbuf()->pubsetbuf(buffer, length); */
 
   fout.open("result.dat");
-  if (!fout.is_open())
-  {
+  if (!fout.is_open()) {
     cout << "Error opening file for result" << endl;
     exit(1);
   }
@@ -137,14 +123,12 @@ inline void Server()
                             block_size, disturb[t]);
 
   for (int i = 1; i < mpi_size; i++)
-    MPI_Irecv(tmp_ans[i], K, MPI_DOUBLE, i, kAns, MPI_COMM_WORLD,
+    MPI_Irecv(tmp_ans[i], K, MPI_FLOAT, i, kAns, MPI_COMM_WORLD,
               &mpi_request[i]);
-  for (int i = 1; i < mpi_size; i++)
-  {
+  for (int i = 1; i < mpi_size; i++) {
     MPI_Wait(&mpi_request[i], &mpi_status);
 #pragma omp parallel for schedule(static)
-    for (int t = 0; t < K; t++)
-      ans[t] += tmp_ans[i][t];
+    for (int t = 0; t < K; t++) ans[t] += tmp_ans[i][t];
   }
 
   const unsigned int length = 4194304;
@@ -154,12 +138,11 @@ inline void Server()
 
   int offset = 0;
 
-  for (int t = 0; t < K; t++)
-  {
+  for (int t = 0; t < K; t++) {
     offset += sprintf(buffer + offset, "%d: %6e\n", t + 1, ans[t]);
   }
 
-  //int len = strlen(buffer);
+  // int len = strlen(buffer);
   fout.write(buffer, offset);
 
   fout.close();
@@ -170,44 +153,41 @@ inline void Server()
       chrono::duration_cast<chrono::microseconds>(endTime - startTime);
   cout << "Computing time=" << compTime.count() << " microseconds" << endl;
 
-  for (int i = 1; i < mpi_size; i++)
-    delete[] tmp_ans[i];
+  for (int i = 1; i < mpi_size; i++) delete[] tmp_ans[i];
 }
 
-inline void Client()
-{
+inline void Client() {
   int m_;
   if (mpi_id == mpi_size - 1)
     m_ = remainder_size;
   else
     m_ = block_size;
-  MPI_Recv(disturb, K, MPI_DOUBLE, 0, kDisturb, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(dat_real, m_, MPI_DOUBLE, 0, kDatReal, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(dat_imag, m_, MPI_DOUBLE, 0, kDatImag, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(pri_real, m_, MPI_DOUBLE, 0, kPriReal, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(pri_imag, m_, MPI_DOUBLE, 0, kPriImag, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(ctf, m_, MPI_DOUBLE, 0, kCtf, MPI_COMM_WORLD, &mpi_status);
-  MPI_Recv(sigRcp, m_, MPI_DOUBLE, 0, kSigRcp, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(disturb, K, MPI_FLOAT, 0, kDisturb, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(dat_real, m_, MPI_FLOAT, 0, kDatReal, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(dat_imag, m_, MPI_FLOAT, 0, kDatImag, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(pri_real, m_, MPI_FLOAT, 0, kPriReal, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(pri_imag, m_, MPI_FLOAT, 0, kPriImag, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(ctf, m_, MPI_FLOAT, 0, kCtf, MPI_COMM_WORLD, &mpi_status);
+  MPI_Recv(sigRcp, m_, MPI_FLOAT, 0, kSigRcp, MPI_COMM_WORLD, &mpi_status);
 
   for (unsigned int t = 0; t < K; t++)
     ans[t] = logDataVSPrior(dat_real, dat_imag, pri_real, pri_imag, ctf, sigRcp,
                             m_, disturb[t]);
 
-  MPI_Send(ans, K, MPI_DOUBLE, 0, kAns, MPI_COMM_WORLD);
+  MPI_Send(ans, K, MPI_FLOAT, 0, kAns, MPI_COMM_WORLD);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
-  dat_real = new double[m];
-  dat_imag = new double[m];
-  pri_real = new double[m];
-  pri_imag = new double[m];
-  ctf = new double[m];
-  sigRcp = new double[m];
-  disturb = new double[K];
-  ans = new double[K];
+  dat_real = new float[m];
+  dat_imag = new float[m];
+  pri_real = new float[m];
+  pri_imag = new float[m];
+  ctf = new float[m];
+  sigRcp = new float[m];
+  disturb = new float[K];
+  ans = new float[K];
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_id);
